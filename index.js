41aa23108cb2c1fbe603aa1b9ff940a793855e7d
Waqas Ali <140514512+waqasali-cs@users.noreply.github.com>
2024-01-16 19:33:33 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+/*   
+*****
+*****
+*****
+*****
+*/
+
+let n = 5; // rows
+// declaration of empty variable
+let string = "";
+
+for(let i = 0; i < n; i++) {
+  for(let j = 0; j < n; j++) {
+    string += "*";
+  }
+  
+  string += "\n";
+}
+console.log(string);
+
+/*
+*****
+*   *
+*   *
+*   *
+*****
+*/
+
+let n = 5;
+let string = "";
+
+for(let i = 0; i < n; i++) { 
+  for(let j = 0; j < n; j++) {
+    if(i === 0 || i === n - 1) {
+      string += "*";
+    }
+    else {
+      if(j === 0 || j === n - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+  }
+ 
+  string += "\n";
+}
+
+console.log(string);
