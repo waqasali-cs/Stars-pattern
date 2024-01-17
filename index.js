ceff104e159dbe708faff2add4aa802bf1e3a3c1
Waqas Ali <140514512+waqasali-cs@users.noreply.github.com>
2024-01-17 22:53:20 +0500

    ADD: right angle triangle

--- index.js
+++ index.js
@@ -48,3 +48,26 @@ for(let i = 0; i < n; i++) {
 }
 
 console.log(string);
+
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  
+  for (let j = 0; j < n - i; j++) {
+    string += " ";
+  }
+ 
+  for (let k = 0; k < i; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
